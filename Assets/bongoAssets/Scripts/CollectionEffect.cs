7b9b35748c4774720f44c9221572e083848b00b6
bongo-Louis <louis.h.bongo@gmail.com>
2026-05-28 15:45:46 +0800

    testing out sound, added sound playing in collection script

--- Assets/bongoAssets/Scripts/CollectionEffect.cs
+++ Assets/bongoAssets/Scripts/CollectionEffect.cs
@@ -8,7 +8,19 @@ public class CollectionEffect : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Point"))
         {
-            points++;
+            points++;   
+            AudioSource sound = other.GetComponent<AudioSource>();
+            if (sound != null)
+            {
+                sound.Play();
+            }
+            // destroy after clip length to allow sound to play
+            Destroy(other.gameObject, sound.clip.length); 
+        }
+
+        else if (other.gameObject.CompareTag("Speed"))
+        {
+            // Handle speed power-up logic
             Destroy(other.gameObject);
         }
     }
